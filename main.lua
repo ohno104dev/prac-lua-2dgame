ca100e026a2163bfa3945b5c5b0225944dda5460
ohno <ohno104dev@gmail.com>
2024-09-07 01:09:14 +0800

    feat: add mouse operation

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
 function love.load()
-	
+	target = {}
+	target.x = 300
+	target.y = 300
+	target.radius = 50
+
+	score = 0
+	timer = 0
+
+	gameFont = love.graphics.newFont(40)
 end
 
 --  detal time
@@ -8,8 +16,23 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.setColor(0, 1, 0)
-	love.graphics.rectangle("fill", 200, 250, 200, 100)
-	love.graphics.setColor(1, 0.63, 0)
-	love.graphics.circle("fill", 300, 200, 100)
+	love.graphics.setColor(1, 0, 0)
+	love.graphics.circle("fill", target.x, target.y, target.radius)
+
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.setFont(gameFont)
+	love.graphics.print(score, 0, 0)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+	if button == 1 then
+		local mouseToTarget = distanceBetween(x, y, target.x,target.y)
+		if mouseToTarget < target.radius then
+			score = score + 1
+		end
+	end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+	return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
 end

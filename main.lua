d2c7da63418c5c4797991a1633d3b72ef68e05ed
ohno <ohno104dev@gmail.com>
2024-09-06 08:02:27 +0800

    feat: drawing shapes

---
+++ main.lua
@@ -0,0 +1,13 @@
+function love.load()
+	
+end
+
+--  detal time
+function love.update(dt)
+	
+end
+
+function love.draw()
+	love.graphics.rectangle("fill", 200, 400, 200, 100)
+	love.graphics.circle("line", 300, 200, 100)
+end

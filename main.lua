4f3ff6ebdf7281e34741eb03d60ed89bcbeda792
ohno <ohno104dev@gmail.com>
2024-09-16 16:19:19 +0800

    feat: add sprite rotation and mouse control

--- main.lua
+++ main.lua
@@ -10,25 +10,29 @@ function love.load()
 	player = {}
 	player.x = love.graphics.getWidth() / 2
 	player.y = love.graphics.getHeight() / 2
-	player.speed = 3
+	player.speed = 180
 end
 
 function love.update(dt)
 	if love.keyboard.isDown("d") then
-		player.x = player.x + player.speed
+		player.x = player.x + player.speed * dt
 	end
 	if love.keyboard.isDown("a") then
-		player.x = player.x - player.speed
+		player.x = player.x - player.speed * dt
 	end
 	if love.keyboard.isDown("w") then
-		player.y = player.y - player.speed
+		player.y = player.y - player.speed * dt
 	end
 	if love.keyboard.isDown("s") then
-		player.y = player.y + player.speed
+		player.y = player.y + player.speed * dt
 	end
 end
 
 function love.draw()
 	love.graphics.draw(sprites.space, 0, 0, 0, love.graphics.getWidth() / sprites.space:getWidth(), love.graphics.getHeight() / sprites.space:getHeight())
-	love.graphics.draw(sprites.player, player.x, player.y, 0, 1.8, 1.8)
+	love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, sprites.player:getWidth()/2,sprites.player:getHeight()/2)
+end
+
+function playerMouseAngle()
+	return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) - (math.pi / 2)
 end

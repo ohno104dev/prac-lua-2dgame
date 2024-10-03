53b4b44f0e0c763ef832b5e1e2b1caee028cde5a
ohno <ohno104dev@gmail.com>
2024-10-03 18:55:48 +0800

    feat: add animations

--- main.lua
+++ main.lua
@@ -1,6 +1,20 @@
 function love.load()
+	anim8 = require 'libraries/anim8/anim8'
+
+	sprites = {}
+	sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
+
+	local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+
+	animations = {}
+	animations.idle = anim8.newAnimation(grid('1-15', 1), 0.05)
+	animations.jump = anim8.newAnimation(grid('1-7', 2), 0.05)
+	animations.run = anim8.newAnimation(grid('1-15', 3), 0.05)
+
+
 	wf = require 'libraries/windfield/windfield'
 	world = wf.newWorld(0, 800, false)
+	world:setQueryDebugDrawing(true)
 	
 	world:addCollisionClass('Platform')
 	world:addCollisionClass('Player'--[[, {ignores = {'Platform'}}]])
@@ -9,6 +23,7 @@ function love.load()
 	player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = "Player"})
 	player:setFixedRotation(true)
 	player.speed = 240
+	player.animation = animations.run
 
 	platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = "Platform"})
 	platform:setType('static')
@@ -34,16 +49,31 @@ function love.update(dt)
 		end
 	end
 	
+	player.animation:update(dt)
 end
 
 function love.draw()
 	world:draw()
+	player.animation:draw(sprites.playerSheet, 0, 0)
 end
 
 
 function love.keypressed(key)
 	if key == 'up' then
-		player:applyLinearImpulse(0, -7000)
+		local colliders = world:queryRectangleArea(player:getX() - 40, player:getY() + 40, 80, 2, {'Platform'})
+		if #colliders > 0 then
+			player:applyLinearImpulse(0, -7000)
+		end
+		
 	end
 	
+end
+
+function love.mousepressed(x, y, button)
+	if button == 1 then
+		local colliders = world:queryCircleArea(x, y, 200, {'Platform', 'Danger'})
+		for i,c in ipairs(colliders) do
+			c:destroy()
+		end
+	end
 end
